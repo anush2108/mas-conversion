0237a11633e7f79901f705537111763b65e89ff6
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-09-03 12:28:57 +0530

    Update AuthContext.jsx

--- A-migration-frontend-A/src/context/AuthContext.jsx
+++ A-migration-frontend-A/src/context/AuthContext.jsx
@@ -9,7 +9,7 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const checkLogin = async () => {
       try {
-        const res = await fetch("http://localhost:8000/auth/account", {
+        const res = await fetch("https://mas-migration-backend-1-open-db.apps.itz-47ubpb.infra01-lb.dal14.techzone.ibm.com/auth/account", {
           credentials: "include",
         });
         if (res.ok) {
