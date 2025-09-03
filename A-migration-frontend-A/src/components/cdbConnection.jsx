ff9807705eb60192851b130a707c874b2cc68857
Raju8115 <117435076+Raju8115@users.noreply.github.com>
2025-09-03 21:06:15 +0530

    Update cdbConnection.jsx

--- A-migration-frontend-A/src/components/cdbConnection.jsx
+++ A-migration-frontend-A/src/components/cdbConnection.jsx
@@ -1,24 +1,11 @@
-// src/components/cdbConnection.jsx
+// cdbConnection.jsx
 import PouchDB from 'pouchdb-browser';
 
-// Hardcoded CouchDB URL and credentials for testing
-const couchdbUrl = "https://couchdb-route-open-db.apps.itz-47ubpb.infra01-lb.dal14.techzone.ibm.com";
-const username = "admin";
-const password = "changeme";
+const couchdbUrl = process.env.REACT_APP_COUCHDB_URL;
 const dbName = "transaction";
 
-// Create PouchDB instance with Basic Auth
 const db = new PouchDB(`${couchdbUrl.replace(/\/$/, '')}/${dbName}`, {
   skip_setup: false,
-  fetch: (url, opts) => {
-    opts.headers.set('Authorization', 'Basic ' + btoa(`${username}:${password}`));
-    return PouchDB.fetch(url, opts);
-  }
 });
-
-// Optional: test connection
-db.info()
-  .then(info => console.log("DB info:", info))
-  .catch(err => console.error("DB connection error:", err));
 
 export default db;
